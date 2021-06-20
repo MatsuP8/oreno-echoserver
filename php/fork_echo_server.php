e8845135218314ddba6061586e0179425052acf8
MatsuP8 <matsumoto.s.s.0805@gmail.com>
2021-06-20 19:08:35 +0900

    fix fork echo server

--- php/fork_echo_server.php
+++ php/fork_echo_server.php
@@ -1,31 +1,52 @@
 <?php
+declare(ticks=1);
 
 $socket = stream_socket_server("tcp://0.0.0.0:8000", $errno, $errstr);
+
+pcntl_signal(SIGINT, sig_handler($socket));
 
 if (!$socket) {
     echo "$errstr ($errno)<br />\n";
 } else {
-    while ($conn = stream_socket_accept($socket)) {
-        $pid = pcntl_fork();
-        if ($pid == -1) {
-            die('fork できません');
-        } elseif ($pid) {
-            echo $pid;
-        } else {
-            echo "client connected.\n";
-            while ($read = fread($conn, 8192)) {
-                echo "message receive: $read";
+    $pids = [];
+    while (true) {
+        $conn = @stream_socket_accept($socket, 5);
+        if ($conn) {
+            $pid = pcntl_fork();
+            if ($pid == -1) {
+                die('fork できません');
+            } elseif ($pid) {
+                $pids[] = $pid;
+            } else {
+                echo "client connected.\n";
+                while ($read = fread($conn, 8192)) {
+                    echo "message receive: $read";
 
-                if (rtrim($read, "\n\r") === 'quit') {
-                    break;
+                    if (rtrim($read, "\n\r") === 'quit') {
+                        break;
+                    }
+
+                    $res = "response: $read";
+                    fwrite($conn, $res, strlen($res));
                 }
-
-                $res = "response: $read";
-                fwrite($conn, $res, strlen($res));
+                fclose($conn);
+                exit(0);
             }
-            fclose($conn);
-            exit(0);
+        } else {
+            foreach ($pids as $i => $pid) {
+                $result = pcntl_waitpid($pid, $status, WNOHANG);
+                if ($result > 0 && pcntl_wifexited($status)) {
+                    unset($pids[$i]);
+                }
+            }
         }
     }
-    fclose($socket);
+}
+
+function sig_handler($socket)
+{
+    return function ($signo) use ($socket) {
+        fclose($socket);
+        exit(0);
+    };
 }
